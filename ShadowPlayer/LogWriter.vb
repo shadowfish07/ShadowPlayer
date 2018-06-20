5f9c00dce02a2e4e814aacf26f323b096563182b
shadowfish <shadowfish@qq.com>
2018-06-20 20:20:17 +0800

    加入LogWriter类，用于写入程序LOG文件

---
+++ ShadowPlayer/LogWriter.vb
@@ -0,0 +1,20 @@
+﻿Public Class LogWriter
+    Dim sw As IO.StreamWriter
+    Public Sub New(logPath As String)
+        sw = New IO.StreamWriter(logPath, True)
+        Trace.Listeners.Add(New TextWriterTraceListener(sw))
+    End Sub
+
+    Public Sub Write(message As String)
+        Trace.WriteLine("[" + Now.ToString + " " + Now.Millisecond + "]" + " " + message)
+        sw.Flush()
+    End Sub
+
+    Public Sub Indent()
+        Trace.Indent()
+    End Sub
+
+    Public Sub Unindent()
+        Trace.Unindent()
+    End Sub
+End Class
--- ShadowPlayer/PlayProgress.vb
+++ ShadowPlayer/PlayProgress.vb
@@ -1,6 +1,6 @@
 ﻿Public Class PlayProgress
     Dim gr As Graphics
-    Dim br As Graphics
+    Dim br As Brush
     Dim backcolor As Color
     Dim frontcolor As Color
     Dim width As Integer
@@ -8,8 +8,11 @@
     Dim fillArea As Region
     Dim fillHeight As Integer
 
-    Public Sub New(target As Object, width As Integer, backcolor As Color, frontcolor As Color)
-
+    Public Sub New(target As Graphics, width As Integer, backcolor As Color, frontcolor As Color)
+        gr = target
+        Me.width = width
+        Me.backcolor = backcolor
+        Me.frontcolor = frontcolor
     End Sub
 
     Function SetYuanHuan(target As Graphics, width As Integer) As Region
