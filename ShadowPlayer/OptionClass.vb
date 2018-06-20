95f8a0588e06b8d634b2176a9ef33e2a0ceb763b
shadowfish <1633742366@qq.com>
2018-06-20 18:55:19 +0800

    OptionClass加入对And/Or/Not的运算符

--- ShadowPlayer/OptionClass.vb
+++ ShadowPlayer/OptionClass.vb
@@ -290,7 +290,7 @@ Public Class OptionClass
             ReDim _Children(ubound)
         End Sub
 
-#Region "=、<>重载"
+#Region "=、<>、And、Or、Not重载"
         Shared Operator =(p1 As Myboolean, p2 As Myboolean)
             Return p1.Value = p2.Value
         End Operator
@@ -314,7 +314,36 @@ Public Class OptionClass
         Shared Operator <>(p1 As Myboolean, p2 As Myboolean)
             Return p1.Value <> p2.Value
         End Operator
+
+        Shared Operator And(p1 As Myboolean, p2 As Myboolean)
+            Return p1.Value And p2.Value
+        End Operator
+
+        Shared Operator And(p1 As Boolean, p2 As Myboolean)
+            Return p1 And p2.Value
+        End Operator
+
+        Shared Operator And(p1 As Myboolean, p2 As Boolean)
+            Return p1.Value And p2
+        End Operator
+
+        Shared Operator Or(p1 As Myboolean, p2 As Myboolean)
+            Return p1.Value Or p2.Value
+        End Operator
+
+        Shared Operator Or(p1 As Boolean, p2 As Myboolean)
+            Return p1 Or p2.Value
+        End Operator
+
+        Shared Operator Or(p1 As Myboolean, p2 As Boolean)
+            Return p1.Value Or p2
+        End Operator
+
+        Shared Operator Not(p1 As Myboolean)
+            Return Not p1.Value
+        End Operator
 #End Region
+
         Public Sub New(Value As Boolean)
             _Value = Value
         End Sub
@@ -383,7 +412,7 @@ Public Class OptionClass
         _afterClose_next = New Myboolean(True)
         _lyricScreen_Open = New Myboolean(True)
         SetOnlyOneTrue({_whenClose_EndOpen, _whenClose_MiniOpen, _whenClose_NothingOpen})
-        SetOnlyOneTrue({_AfterClose_next, _AfterClose_puase, _AfterClose_stop})
+        SetOnlyOneTrue({_afterClose_next, _afterClose_puase, _afterClose_stop})
         _alarm_LoudTime = "13:50:35"
         _alarm_EndTime = "13:54:20"
     End Sub
