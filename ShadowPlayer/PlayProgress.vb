8bcdff2fac1f9a37cd0c8bae630765c9044beda5
Shadow Fish <shadowfish_z@qq.com>
2018-06-20 20:58:06 +0800

    添加PlayProgress类的属性和方法

--- ShadowPlayer/PlayProgress.vb
+++ ShadowPlayer/PlayProgress.vb
@@ -1,3 +1,32 @@
 ﻿Public Class PlayProgress
+    Dim gr As Graphics
+    Dim br As Graphics
+    Dim backcolor As Color
+    Dim frontcolor As Color
+    Dim width As Integer
+    Dim yuanHuan As Region
+    Dim fillArea As Region
+    Dim fillHeight As Integer
+
+    Public Sub New(target As Object, width As Integer, backcolor As Color, frontcolor As Color)
+
+    End Sub
+
+    Function SetYuanHuan(target As Graphics, width As Integer) As Region
+
+    End Function
+
+    Public Sub Draw()
+
+    End Sub
+
+    Function GetFillArea() As Region
+
+    End Function
+
+    Public Function IsinYuanHuan() As Boolean
+
+    End Function
+
 
 End Class
